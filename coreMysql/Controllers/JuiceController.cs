24f5877a023f304abfda6ddcaa76732ecc9b4c3c
kuna <prasanthkuna@github.com>
2018-04-17 17:33:00 +0530

    .net sample

---
+++ coreMysql/Controllers/JuiceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using coreMysql.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreMysql.Controllers
+{
+    [Route("api/[controller]")]
+    public class JuiceController:Controller
+    {
+        private readonly JuiceContext db;
+        public JuiceController(JuiceContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public JsonResult GetBugs()
+        {
+            
+            var result = db.Bug.Select(x => x).ToList();
+            return Json(result);
+        }
+    }
+}
---
+++ coreMysql/Model/JuiceModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace coreMysql.Model
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+
+    }
+}
